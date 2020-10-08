123e1f9f4552bfc70328aa5b8016ffdcf4602df9
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-10-08 11:08:08 +0700

    Add files via upload

---
+++ tugas6_1.php
@@ -0,0 +1,12 @@
+<?php
+$Habib = 21;
+$Aji = $Habib + 4;
+$Bintan = $Aji + 3;
+
+echo "====================================== <br>";
+echo "Usia Habib = " .$Habib. "<br>";
+echo "Usia Aji ="  .$Aji. "<br>";
+echo "Usia Bintan = " .$Bintan. "<br>";
+echo "====================================== <br>";
+
+?>
